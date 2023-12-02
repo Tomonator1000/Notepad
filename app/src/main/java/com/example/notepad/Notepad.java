148fbbf1db2bb654168ff89acefe3364498915eb
tommy <tommysmith12443@gmail.com>
2023-12-02 16:52:21 -0700

    Notepad class

---
+++ app/src/main/java/com/example/notepad/Notepad.java
@@ -0,0 +1,22 @@
+package com.example.notepad;
+
+public class Notepad {
+    private String title = "";
+    private String note = "";
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
