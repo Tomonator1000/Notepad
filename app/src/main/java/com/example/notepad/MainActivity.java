4eab5ac618338ce9c3b275e97bcdd9fba285a002
Ethan Townsend <ethantownsend73@gmail.com>
2023-12-02 16:45:48 -0700

    ui finished

--- app/src/main/java/com/example/notepad/MainActivity.java
+++ app/src/main/java/com/example/notepad/MainActivity.java
@@ -2,18 +2,39 @@ package com.example.notepad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnCreateNote;
+    ScrollView scrollViewNotes;
+    LinearLayout buttonLayout;
+    Intent NotepadActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onInit();
     }
 
-    public void onInIt(){
-        
+    public void onInit(){
+        btnCreateNote = findViewById(R.id.btnNewNote);
+        scrollViewNotes = findViewById(R.id.buttonScrollView);
+        buttonLayout = findViewById(R.id.buttonLayout);
+
+        NotepadActivity = new Intent(this,NotepadActivity.class);
+    }
+
+    public void createNote(View v){
+        //TODO: BUTTON CREATES NEW NOTE BUTTON.
+        NotepadActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(NotepadActivity);
     }
 
 }
--- app/src/main/java/com/example/notepad/NotepadActivity.java
+++ app/src/main/java/com/example/notepad/NotepadActivity.java
@@ -3,13 +3,25 @@ package com.example.notepad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NotepadActivity extends AppCompatActivity {
+    EditText noteName, noteNote;
+    Button noteSave, noteCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notepad);
+        onInit();
+    }
+
+    protected void onInit(){
+        noteName = findViewById(R.id.etNoteName);
+        noteNote = findViewById(R.id.etNoteNote);
+        noteSave = findViewById(R.id.btnSaveEditNote);
+        noteCancel = findViewById(R.id.btnCancelEditNote);
     }
 
 
